cdd6beea08ac4b88c9cc3ba1fe46f9f961d30170
Nguyễn Quang Khải <124948877+nqkhai294@users.noreply.github.com>
2024-04-12 08:50:28 +0700

    ArrowFunction
    s
    
    a
    sd
    á
    out

---
+++ ArrowFunction/arrowFun.js
@@ -0,0 +1,13 @@
+//function
+function SubmitEvent(a, b) {
+    return a + b;
+}
+
+// Arrow Function
+
+let sumA = (a, b) => {
+    return a + b;
+}
+
+console.log('check sum: ', sum(6, 9));
+console.log('check sum: ', sumA(6, 9)); 
